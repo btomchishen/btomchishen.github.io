9f41e3ffabcc53ee8d9f900e10b22dc85fefae65
btomchishen <51472786+btomchishen@users.noreply.github.com>
2019-06-08 21:34:20 +0300

    Upd 3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-menu.onclick = function myFuncrion() {
+menu.onclick = function myFunction() {
 	var x = document.getElementById("myTopnav");
 	if (x.className === "topnav") {
 		x.className += "responsive";
